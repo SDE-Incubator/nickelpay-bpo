85f9cbd15acf8c4d2584a7a6f1fc2745f56cecc0
Tainan Nery <zaoldyeck@outlook.com>
2023-03-01 09:34:25 -0300

    feat: added react-query-devtools

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,13 @@
-import type { AppProps } from "next/app";
-import {
-  QueryClient,
-  Hydrate,
-  QueryClientProvider,
-} from "@tanstack/react-query";
-import { ThemeProvider } from "styled-components";
-import GlobalStyles from "../styles/globalStyles";
-import theme from "../styles/theme";
+import type {AppProps} from 'next/app'
+import {QueryClient, Hydrate, QueryClientProvider} from '@tanstack/react-query'
+import {ReactQueryDevtools} from '@tanstack/react-query-devtools'
+import {ThemeProvider} from 'styled-components'
+import GlobalStyles from '../styles/globalStyles'
+import theme from '../styles/theme'
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient()
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({Component, pageProps}: AppProps) {
   return (
     <>
       <GlobalStyles />
@@ -18,9 +15,10 @@ export default function App({ Component, pageProps }: AppProps) {
         <QueryClientProvider client={queryClient}>
           <Hydrate state={pageProps.dehydratedState}>
             <Component {...pageProps} />
+            <ReactQueryDevtools />
           </Hydrate>
         </QueryClientProvider>
       </ThemeProvider>
     </>
-  );
+  )
 }

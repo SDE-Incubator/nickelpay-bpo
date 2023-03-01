ea88302776627f0ac2e3a53625ec0c64b71dda0a
Tainan Nery <zaoldyeck@outlook.com>
2023-03-01 09:35:53 -0300

    fix: login services env

--- src/services/authentication/login/login.service.ts
+++ src/services/authentication/login/login.service.ts
@@ -1,14 +1,14 @@
-import { apiClient } from '../../apiClient'
-import { LoginRequestProps } from './login'
+import {apiClient} from '../../apiClient'
+import {LoginRequestProps} from './login'
 
-async function login({ username, password }: LoginRequestProps) {
-  const { data } = await apiClient({
+async function login({username, password}: LoginRequestProps) {
+  const {data} = await apiClient({
     method: 'POST',
     base: 'access',
     url: '/login',
-    data: { username, password },
+    data: {username, password},
     headers: {
-      'Tenant-Key': process.env.TENANT_ID,
+      'Tenant-Key': process.env.NEXT_PUBLIC_TENANT_KEY,
       platform_enabled: 'backoffice',
     },
   })
@@ -16,4 +16,4 @@ async function login({ username, password }: LoginRequestProps) {
   return data
 }
 
-export { login }
+export {login}
